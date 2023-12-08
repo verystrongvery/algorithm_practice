53ddfaddfd57b0e5278e983824a46d0ee8fbd62a
verystrongvery <jinbkim42@gmail.com>
2023-12-08 19:48:31 +0900

    fix: Greedy - 프로그래머스: 점프와 순간이동

--- greedy/programmers/점프와 순간 이동/Solution.java
+++ greedy/programmers/점프와 순간 이동/Solution.java
@@ -1,13 +1,13 @@
-import java.util.*;
-
-public class Solution {
+class Solution {
     public int solution(int n) {
         int answer = 0;
-        while (n > 0) {
-            if (n % 2 != 0) {
-                answer++;
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+                continue;
             }
-            n /= 2;
+            answer++;
+            n--;
         }
         return answer;
     }
