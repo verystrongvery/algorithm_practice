d780d873a6c19b97faf671c83205600bbffc4534
verystrongvery <jinbkim42@gmail.com>
2023-12-09 20:34:21 +0900

    fix: Queue - 프로그래머스: [1차] 캐시

--- datastructure/queue/programmers/[1차] 캐시/Solution.java
+++ datastructure/queue/programmers/[1차] 캐시/Solution.java
@@ -1,42 +1,24 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
 
 class Solution {
-    Queue<String> q = new LinkedList<>();
-    int answer = 0;
-
     public int solution(int cacheSize, String[] cities) {
-        if (cacheSize == 0) {
-            return cities.length * 5;
-        }
+        int answer = 0;
+        Queue<String> q = new LinkedList<>();
         for(String city : cities) {
-            chooseUseCache(city.toLowerCase(), cacheSize);
-        }
-        return answer;
-    }
-
-    private void chooseUseCache(String city, int cacheSize) {
-        if (q.contains(city)) {
-            useCache(city);
-            return ;
-        }
-        if(q.size() == cacheSize){
-            q.poll();
-        }
-        q.offer(city);
-        answer += 5;
-    }
-
-    private void useCache(String cachedCity) {
-        Queue<String> newQ = new LinkedList<>();
-        while(!q.isEmpty()) {
-            String city = q.poll();
-            if (city.equals(cachedCity)) {
+            city = city.toLowerCase();
+            if (q.size() > cacheSize) {
+                q.poll();
+            }
+            if (q.contains(city)) {
+                answer += 1;
+                q.remove(city);
+                q.offer(city);
                 continue;
             }
-            newQ.offer(city);
+            q.add(city);
+            answer += 5;
         }
-        newQ.offer(cachedCity);
-        q = newQ;
-        answer++;
+        return answer;
     }
 }
