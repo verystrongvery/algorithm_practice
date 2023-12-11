9c459077a1afe40d934645ecbfc4e138b9fcdddb
verystrongvery <jinbkim42@gmail.com>
2023-12-11 10:13:42 +0900

    fix: String - 프로그래머스: k진수에서 소수 개수 구하기

--- string/programmers/k진수에서 소수 개수 구하기/ Solution.java
+++ string/programmers/k진수에서 소수 개수 구하기/ Solution.java
@@ -2,14 +2,14 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int n, int k) {
-        return (int)Arrays.stream(String.valueOf(Long.toString(n, k)).split("0"))
-                .filter(str -> !str.equals(""))
-                .filter(str -> isPrime(Long.parseLong(str)))
+        return (int)Arrays.stream(Long.toString(n, k).split("0"))
+                .filter(s -> !s.equals(""))
+                .filter(s -> isPrime(Long.parseLong(s)))
                 .count();
     }
 
     private boolean isPrime(long n) {
-        if (n == 1) {
+        if (n < 2) {
             return false;
         }
         for(long i = 2; i * i <= n; i++) {
