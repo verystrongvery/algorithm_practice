b203980d3af00d4f69c6aa148b1d7ee2ff14c9ea
verystrongvery <jinbkim42@gmail.com>
2023-12-12 20:06:48 +0900

    fix: String - 프로그래머스: [3차] 압축

--- string/programmers/[3차] 압축/Solution.java
+++ string/programmers/[3차] 압축/Solution.java
@@ -1,37 +1,45 @@
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(String msg) {
-        List<String> dictionary = init();
-        List<Integer> answer = new ArrayList();
-        while(msg.length() != 0) {
-            msg = compression(msg, dictionary, answer);
-        }
+        List<String> dictionary = initDictionary();
+        List<String> answer = solve(dictionary, msg);
         return answer.stream()
-                .mapToInt(Integer::intValue)
+                .mapToInt(str -> dictionary.indexOf(str) + 1)
                 .toArray();
     }
 
-    private List<String> init() {
+    private List<String> initDictionary() {
         return IntStream.range(0, 26)
                 .mapToObj(i -> String.valueOf((char)('A' + i)))
                 .collect(Collectors.toList());
     }
 
-    private String compression(String msg, List<String> dictionary, List<Integer> answer) {
-        String msgSubStr = "";
-        for(int i = 1; i <= msg.length(); i++) {
-            msgSubStr = msg.substring(0, i);
-            if (!dictionary.contains(msgSubStr)) {
-                dictionary.add(msgSubStr);
-                answer.add(dictionary.indexOf(msg.substring(0, i - 1)) + 1);
-                return msg.substring(i - 1, msg.length());
+    private List<String> solve(List<String> dictionary, String msg) {
+        List<String> answer = new ArrayList<>();
+        int i = 0;
+        while (i < msg.length()) {
+            int j = i + 1;
+            while (j <= msg.length()) {
+                String subStr = msg.substring(i, j);
+                if (j == msg.length() && dictionary.contains(subStr)) {
+                    answer.add(subStr);
+                    j = msg.length() + 1;
+                    break ;
+                }
+                else if (dictionary.contains(subStr)) {
+                    j++;
+                    continue;
+                }
+                dictionary.add(subStr);
+                answer.add(subStr.substring(0, subStr.length() - 1));
+                break ;
             }
+            i = j - 1;
         }
-        answer.add(dictionary.indexOf(msgSubStr) + 1);
-        return "";
+        return answer;
     }
 }
