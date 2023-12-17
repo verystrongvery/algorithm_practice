9494ac67af70028e4d26fe8ff1c3ce1defb95834
verystrongvery <jinbkim42@gmail.com>
2023-12-18 05:51:25 +0900

    fix: Set - 프로그래머스: 방문 길이

--- datastructure/set/programmers/방문 길이/Solution.java
+++ datastructure/set/programmers/방문 길이/Solution.java
@@ -1,37 +1,66 @@
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Objects;
+
+class Pos {
+    int x;
+    int y;
+    int nextX;
+    int nextY;
+
+    public Pos(int x, int y, int nextX, int nextY) {
+        this.x = x;
+        this.y = y;
+        this.nextX = nextX;
+        this.nextY = nextY;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Pos pos = (Pos)o;
+        if (x == pos.x && y == pos.y && nextX == pos.nextX && nextY == pos.nextY) {
+            return true;
+        }
+        return false;
+    }
+
+    public int hashCode() {
+        return Objects.hash(x, y, nextX, nextY);
+    }
+}
 
 class Solution {
+    int x = 0;
+    int y = 0;
     public int solution(String dirs) {
-        Set<String> set = move(dirs);
+        Set<Pos> set = new HashSet<>();
+        for(String dir : dirs.split("")) {
+            move(set, dir);
+        }
         return set.size() / 2;
     }
 
-    private Set<String> move(String dirs) {
-        int posX = 0;
-        int posY = 0;
-        Set<String> set = new HashSet<>();
-        for(char dir : dirs.toCharArray()) {
-            int curX = posX;
-            int curY = posY;
-            if (dir == 'U' && curY < 5) {
-                posY++;
-            }
-            else if (dir == 'D' && curY > -5) {
-                posY--;
-            }
-            else if (dir == 'L' && curX > -5) {
-                posX--;
-            }
-            else if (dir == 'R' && curX < 5) {
-                posX++;
-            }
-            else {
-                continue;
-            }
-            set.add("" + curX + curY + posX + posY);
-            set.add("" + posX + posY + curX + curY);
+    public void move(Set<Pos> set, String dir) {
+        int nextX = x;
+        int nextY = y;
+        if (dir.equals("U") && y + 1 <= 5) {
+            nextY++;
         }
-        return set;
+        else if (dir.equals("D") && y - 1 >= -5) {
+            nextY--;
+        }
+        else if (dir.equals("L") && x - 1 >= -5) {
+            nextX--;
+        }
+        else if (dir.equals("R") && x + 1 <= 5){
+            nextX++;
+        }
+        if (x == nextX && y == nextY) {
+            return ;
+        }
+        set.add(new Pos(x, y, nextX, nextY));
+        set.add(new Pos(nextX, nextY, x, y));
+        x = nextX;
+        y = nextY;
     }
 }
