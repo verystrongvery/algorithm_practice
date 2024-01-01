2ecbcec4f10b663a7ce7685c9a58a26e60cf1b7e
verystrongvery <jinbkim42@gmail.com>
2024-01-01 14:55:49 +0900

    fix: String - 프로그래머스: 스킬트리

--- string/programmers/스킬트리/Solution.java
+++ string/programmers/스킬트리/Solution.java
@@ -1,27 +1,30 @@
+// 1. 해당 스킬트리가 skill에 존재하는지 확인
+
+// 2.1 존재하는데 skill의 맨 앞글자가 아닌경우, 불가능한 스킬트리
+// 2.2 존재하는데 skill의 맨 앞글자인 경우, skill의 맨 앞글자를 지우고, 다음 스킬트리 조회
+
+// 3. 존재하지 않는 경우, 다음 스킬트리 조회
+
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        for(String skillTree : skill_trees) {
-            if (isPossible(skill, skillTree)) {
+        for(String st : skill_trees) {
+            if (isPossibleSkill(skill, st)) {
                 answer++;
             }
         }
         return answer;
     }
 
-    private boolean isPossible(String skill, String skillTree) {
-        int i = 0;
+    public boolean isPossibleSkill(String skill, String skillTree) {
         for(String st : skillTree.split("")) {
-            if (i == skill.length()) {
-                break;
+            if (!skill.contains(st)) {
+                continue;
             }
-            String substring = skill.substring(i + 1, skill.length());
-            if (substring.contains(st)) {
+            if (skill.charAt(0) != st.charAt(0)) {
                 return false;
             }
-            if (st.charAt(0) == skill.charAt(i)) {
-                i++;
-            }
+            skill = skill.substring(1);
         }
         return true;
     }
