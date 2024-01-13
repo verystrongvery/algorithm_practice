44dde87badc65af95cba69474b3a075c54ac1181
verystrongvery <jinbkim42@gmail.com>
2024-01-13 20:33:49 +0900

    feat: String - 프로그래머스: 2개 이하로 다른 비트

--- string/programmers/2개 이하로 다른 비트/Solution.java
+++ string/programmers/2개 이하로 다른 비트/Solution.java
@@ -1,29 +1,29 @@
+// 1. 10진수를 2진수로 변환
+// 2. 2진수의 작은 비트부터 처음으로 0이 나오는 자리수 찾기
+// 3. 처음으로 0이 나오는 자리수를 1로 바꾸기
+// 4. 처음으로 0이 나오는 자리수 전의 자리가 존재하면 0으로 바꾸기
+
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
     public long[] solution(long[] numbers) {
         return Arrays.stream(numbers)
-                .map(this::solve)
+                .boxed()
+                .map(Long::toBinaryString)
+                .map(this::convert)
+                .mapToLong(str -> Long.valueOf(str, 2))
                 .toArray();
     }
 
-    private long solve(long number) {
-        String binaryString = Long.toBinaryString(number);
-        int i = binaryString.length();
-        while(true) {
-            if (--i < 0) {
-                binaryString = "10" + binaryString.substring(1, binaryString.length());
-                break;
-            }
-            if (binaryString.charAt(i) == '0' && i != binaryString.length() - 1) {
-                binaryString = binaryString.substring(0, i) + "10" + binaryString.substring(i + 2, binaryString.length());
-                break ;
-            }
-            if (binaryString.charAt(i) == '0' && i == binaryString.length() - 1) {
-                binaryString = binaryString.substring(0, binaryString.length() - 1) + "1";
-                break ;
-            }
+    private String convert(String number) {
+        int index = number.lastIndexOf('0');
+        if (index == number.length() - 1) {
+            return number.substring(0, number.length() - 1) + "1";
         }
-        return Long.parseLong(binaryString, 2);
+        if (index == -1) {
+            return "10" + number.substring(1);
+        }
+        return number.substring(0, index) + "10" + number.substring(index + 2);
     }
 }
