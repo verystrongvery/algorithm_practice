f814d7de8b279fd5cb3244c1e853ee2f8948b312
verystrongvery <jinbkim42@gmail.com>
2024-01-14 14:47:19 +0900

    fix: Implementation - 프로그래머스: 삼각 달팽이

--- implementation/programmers/삼각 달팽이/Solution.java
+++ implementation/programmers/삼각 달팽이/Solution.java
@@ -1,31 +1,35 @@
+// 1. 아래로 n칸 이동하면서 1씩 증가
+// 2. 오른쪽으로 n-1칸 이동하면서 1씩 증가
+// 3. 왼쪽위로 n-2칸 이동하면서 1씩 증가
+// 4. 모두 그릴때 까지 반복
+
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int n) {
-        int map[][] = new int[n][n];
+        int[][] arr = new int[n][n];
         int x = 0;
         int y = -1;
         int num = 0;
-        for(int i = 0; i < n ; i++) {
+        for(int i = 0; i < n; i++) {
             for(int j = 0; j < n - i; j++) {
-                if(i % 3 == 0) {
-                    y++;
+                if (i % 3 == 0) {
+                    arr[++y][x] = ++num;
                 }
-                else if(i % 3 == 1) {
-                    x++;
+                else if (i % 3 == 1) {
+                    arr[y][++x] = ++num;
                 }
                 else {
-                    x--;
-                    y--;
+                    arr[--y][--x] = ++num;
                 }
-                map[y][x] = ++num;
             }
         }
         List<Integer> list = new ArrayList<>();
         for(int i = 0; i < n; i++) {
             for(int j = 0; j <= i; j++) {
-                list.add(map[i][j]);
+                list.add(arr[i][j]);
             }
         }
         return list.stream()
