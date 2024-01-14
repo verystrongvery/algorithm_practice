26e7699581362dff710a214c4b19d9e81bbd79bf
verystrongvery <jinbkim42@gmail.com>
2024-01-14 14:18:00 +0900

    feat: Queue - 프로그래머스: 두 큐 합 같게 만들기

--- datastructure/queue/programmers/두 큐 합 같게 만들기/Solution.java
+++ datastructure/queue/programmers/두 큐 합 같게 만들기/Solution.java
@@ -1,46 +1,42 @@
-import java.util.Queue;
+// 1. 큐의 합이 큰쪽에서 원소를 빼고, 큐의 합이 작은쪽으로 원소 넣기
+// 2. 두 큐의 크기의 합만큼 원소를 빼고 넣었는데 답을 구하지 못했다면 -1 반환
+
 import java.util.Arrays;
+import java.util.Queue;
 import java.util.LinkedList;
-import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
+        long sum1 = Arrays.stream(queue1).sum();
+        long sum2 = Arrays.stream(queue2).sum();
+        Queue<Integer> q1 = initQueue(queue1);
+        Queue<Integer> q2 = initQueue(queue2);
         int answer = 0;
-        Queue<Long> q1 = toQueue(queue1);
-        Queue<Long> q2 = toQueue(queue2);
-        int limit = q1.size() * 3;
-        long sum1 = computeSum(q1);
-        long sum2 = computeSum(q2);
-        for (int i = 0; i <= limit; i++) {
+        for(int i = 0; i < (queue1.length + queue2.length) * 2; i++) {
             if (sum1 == sum2) {
                 return answer;
             }
             answer++;
             if (sum1 > sum2) {
-                long num = q1.poll();
-                q2.offer(num);
-                sum1 -= num;
-                sum2 += num;
+                int element = q1.poll();
+                q2.offer(element);
+                sum1 -= element;
+                sum2 += element;
                 continue;
             }
-            long num = q2.poll();
-            q1.offer(num);
-            sum2 -= num;
-            sum1 += num;
+            int element = q2.poll();
+            q1.offer(element);
+            sum2 -= element;
+            sum1 += element;
         }
         return -1;
     }
 
-    private Queue<Long> toQueue(int[] intArr) {
-        return Arrays.stream(intArr)
-                .mapToLong(l -> (long)l)
-                .boxed()
-                .collect(Collectors.toCollection(LinkedList::new));
-    }
-
-    private long computeSum(Queue<Long> q) {
-        return q.stream()
-                .mapToLong(l -> l)
-                .sum();
+    private Queue<Integer> initQueue(int[] queue) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int element : queue) {
+            q.offer(element);
+        }
+        return q;
     }
 }
